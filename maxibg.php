4c0bf3e160c33235dd4fc2d800798d3d2ba3f9b2
Jose Erlino Lontok <jelontok@gmail.com>
2014-06-29 23:14:54 +0800

    Update maxibg.php
    updated post update messages

--- maxibg.php
+++ maxibg.php
@@ -221,8 +221,8 @@
                     3 => __( 'Custom field deleted.', 'maxibg' ),
                     4 => sprintf( __( '%s updated.', 'maxibg' ), esc_attr( $singular ) ),
                     5 => isset( $_GET['revision']) ? sprintf( __('%2$s restored to revision from %1$s', 'maxibg' ), wp_post_revision_title( (int) $_GET['revision'], false ), esc_attr( $singular ) ) : false,
-                    6 => sprintf( __( '%s published. <a href="%s">View %s</a>'), $singular, esc_url( get_permalink( $post_ID ) ), strtolower( $singular ) ),
-                    7 => sprintf( __( '%s saved.', 'maxibg' ), esc_attr( $singular ) ),
+                    6 => sprintf( __( 'Slideshow background saved. Shortcode is [maxibg id="'.$post_ID.'"]', 'maxibg' ), $post->post_name),
+                    7 => sprintf( __( 'saved. Shortcode is [maxibg id="'.$post_ID.'"]', 'maxibg' ), $post->post_name),
                     8 => sprintf( __( '%s submitted. <a href="%s" target="_blank">Preview %s</a>'), $singular, esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ), strtolower( $singular ) ),
                     9 => sprintf( __( '%s scheduled for: <strong>%s</strong>. <a href="%s" target="_blank">Preview %s</a>' ), $singular, date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ), strtolower( $singular ) ),
                     10 => sprintf( __( '%s draft updated. <a href="%s" target="_blank">Preview %s</a>'), $singular, esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ), strtolower( $singular ) )
